2656b543fc66225d3b58d98bb87f6c7c843eb2ea
Triparna Singha <138139515+trips8619@users.noreply.github.com>
2023-10-02 20:01:43 +0530

    Update iterator in vector.cpp

--- iterator in vector.cpp
+++ iterator in vector.cpp
@@ -2,7 +2,11 @@
 
 #include<iostream>
 #include <vector>
-using namespace std;		
+using namespace std;
+void display(vector<int> &v){
+	for (int i = 0; i<v.size();i++){
+		cout<<v[i]<<" ";
+		
 	}
 	cout<<endl;
 }
