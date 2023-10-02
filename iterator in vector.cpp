fe8e9bc3509977b4bb48f1d791aa6b8db6e78e5c
ASMITA MALLICK <asmitamallick2@gmail.com>
2023-10-02 19:59:18 +0530

    Create iterator in vector.cpp

---
+++ iterator in vector.cpp
@@ -0,0 +1,25 @@
+//iterator in vector
+
+#include<iostream>
+#include <vector>
+using namespace std;		
+	}
+	cout<<endl;
+}
+int main(){
+	vector<int> vec1;
+	int element,size;
+	cout<<"enter the size of your vector "<<endl;
+	cin>>size;
+	for(int i = 0; i<size; i++){
+		cout<<"enter an element to add to this vector "<<endl;
+		cin>>element;
+		vec1.push_back(element);
+	}
+	//vec1.pop_back();
+	display(vec1);
+	vector<int> :: iterator iter = vec1.begin();
+	vec1.insert(iter+1,50, 43);
+	display(vec1);
+	return 0;
+}
